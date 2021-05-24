b0da8f653f951bcb8a813b51a07ee5bdc8755715
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-05-24 11:10:28 +0200

    passed all, cleaned up the code as well

--- server.js
+++ server.js
@@ -8,29 +8,16 @@ const passport = require('passport');
 const routes = require('./routes');
 const auth = require('./auth.js');
 
-// const passportSocketIo = require('passport.socketio');
-// const MongoStore = require('connect-mongo');
-// const cookieParser = require('cookie-parser');
-
 const app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
-// const MongoStore = require('connect-mongo')(session);
-// const URI = process.env.MONGO_URI;
-// const store = new MongoStore({ url: URI });
-
-// io.use(
-//   passportSocketIo.authorize({
-//     cookieParser: cookieParser,
-//     key: 'express.sid',
-//     secret: process.env.SESSION_SECRET,
-//     store: store,
-//     success: onAuthorizeSuccess,
-//     fail: onAuthorizeFail
-//   })
-// );
+const passportSocketIo = require('passport.socketio');
+//const MongoStore = require('connect-mongo');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 app.set('view engine', 'pug');
 
@@ -44,10 +31,24 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
@@ -56,14 +57,31 @@ myDB(async (client) => {
 //all the events that a socket can emit are handled in connection
 //if you call it on io (or server) you send it to all the clients, sockets 
 //send it to the client that you are working with
+    socket.on('chat message', (message) => {
+      io.emit('chat message', (name, message) => {
+        name,
+        message
+      })
+    }) 
+
 
     currentUsers += 1;
-    io.emit('user count', currentUsers);
+    io.emit('user', {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true
+    });
     console.log('A user has connected');
-    
+//now I can get the socket info
+    console.log('user ' + socket.request.user.name + ' connected');
     socket.on('disconnect', () => {
       currentUsers -=1;
       console.log('A user has disconnected')
+      io.emit('user', {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false
+      });
     });
   });
   
@@ -76,6 +94,19 @@ myDB(async (client) => {
     res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
+
+
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 http.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port ' + process.env.PORT);
